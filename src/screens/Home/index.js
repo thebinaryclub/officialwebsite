3c758d5634cbc707c8f7cf6a74f7e48f9479a5e4
Aditya <learning.digital558@gmail.com>
2022-05-05 17:34:43 +0000

    added team section and changed main color

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Navbar from '../../components/Navbar';
 import Hero from '../../components/Hero';
 import About from '../../components/About';
+// import Sidebar from '../../components/Sidebar';
+import { Team } from '../../components/Team';
 export const Home = () => {
   return (
     <div>
         <Navbar />
         <Hero />
         <About />
+        <Team />
     </div>
   )
 }
